68795a92344fc4773e20f34099e1fbfda0c4ea64
atocchi <andrewtocchi@gmail.com>
2020-01-24 08:53:17 -0800

    change seeds to reflect new expected data in DB via server side changes

--- seeds.sql
+++ seeds.sql
@@ -1,13 +1,13 @@
 INSERT INTO files (image, user)
 VALUES 
-('http://localhost:3001/q.PNG','anon'),
-('http://localhost:3001/q.PNG','anon'),
-('http://localhost:3001/q.PNG','anon'),
-('http://localhost:3001/q.PNG','anon'),
-('http://localhost:3001/q.PNG','anon'),
-('http://localhost:3001/q.PNG','anon'),
-('http://localhost:3001/q.PNG','anon'),
-('http://localhost:3001/q.PNG','anon'),
-('http://localhost:3001/q.PNG','anon'),
-('http://localhost:3001/q.PNG','anon');
+('q.PNG','anon'),
+('q.PNG','anon'),
+('q.PNG','anon'),
+('q.PNG','anon'),
+('q.PNG','anon'),
+('q.PNG','anon'),
+('q.PNG','anon'),
+('q.PNG','anon'),
+('q.PNG','anon'),
+('q.PNG','anon'),
 
