b73ab1a2ec12f5e4f483c9e4e3347c41e087edaf
atocchi <andrewtocchi@gmail.com>
2020-02-06 10:57:09 -0800

    adding build, removed gitignore'

---
+++ Client/jubjub/build/precache-manifest.c67610fe958809346e516bfe7ff91f3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe4923cfb8c23e2a51d4ac50dba63cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b062554ab93815e811f",
+    "url": "./static/css/main.ddbdbc56.chunk.css"
+  },
+  {
+    "revision": "f59133fb4984f5a3df7e",
+    "url": "./static/js/2.e8ee327a.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "./static/js/2.e8ee327a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b062554ab93815e811f",
+    "url": "./static/js/main.40b6dfad.chunk.js"
+  },
+  {
+    "revision": "4d1fe0e3852155a1bf64",
+    "url": "./static/js/runtime-main.171eeded.js"
+  },
+  {
+    "revision": "2b71dc6d3e5be3552e683cb12d0dd951",
+    "url": "./static/media/defocused.2b71dc6d.jpg"
+  }
+]);
